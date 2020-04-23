37e3d49a9037bdd4be1e3ad13479e4f8ae7fcbe2
unknown <leon.brandt@student.uclouvain.be>
2020-04-23 09:05:11 +0200

    Specs

---
+++ Gauss-Jordan.py
@@ -0,0 +1,16 @@
+"""Brandt Leon, Claerhout Julien
+
+LINFO1112-Algèbre: Challenge 2
+Gaussian Elimination
+"""
+
+def gauss_jordan(A):
+    """
+Should return the Gauss-Jordan Eliminated matrix
+
+    :param A: Matrix of n*m size, n>1, m>1
+    :return: Reduced matrix, same size
+    """
+
+
+
